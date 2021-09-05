5c8716516a10d115035733a1348aee7a31328d7b
Marco8301 <marcanmalas@gmail.com>
2021-09-05 15:17:08 +0200

    🗃 Create Question Entity

---
+++ src/Entity/Traits/SluggableTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+trait SluggableTrait
+{
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function createSlug(): void
+    {
+        $this->setSlug((new AsciiSlugger())->slug($this->getTitle()));
+
+    }
+}
